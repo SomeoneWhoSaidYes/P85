d3e19a44a3c5e154397967fb5c167a5496e51d9c
SomeoneWhoSaidYes <92833774+SomeoneWhoSaidYes@users.noreply.github.com>
2022-02-11 20:11:34 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,118 @@
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+//Create a reference for canvas 
+
+
+//Give specific height and width to the car image
+grencar_width = 75;
+greencar_height = 100;
+
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
+
+//Set initial position for a car image.
+greencar_x = 5;
+greencar_y = 225;
+
+function add() {
+	background_imgTag = new Image();
+	background_imgTag.onload = uploadBackground;
+	bacground_imgTag.src = background_image;
+
+	greencar_imgTag = new Image();
+	greencar_imgTag.onload = uploadgreencar;
+	greencar_imgTag.src = greencar_image;
+}
+
+function uploadBackground() {
+	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+
+}
+
+function uploadgreencar() {
+		ctx.drawImage(greencar_imgTag, greencar_x, greencar_y, greencar_width, greencar_height);
+
+	
+}
+
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		
+}
+
+function up()
+{
+	if(player_y >=0)
+    {
+        player_y = player_y - block_image_height;
+        console.log("block image height = " + block_image_height);
+        console.log("When up arrow key is pressed, X =  " + player_x + " , Y = "+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function down()
+{
+	if(player_y <=500)
+    {
+        player_y = player_y + block_image_height;
+        console.log("block image height = " + block_image_height);
+        console.log("When Down arrow key is pressed, X =  " + player_x + " , Y = "+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function left()
+{
+	if(player_x >=0)
+    {
+        player_x = player_x - block_image_width;
+        console.log("block image width = " + block_image_width);
+        console.log("When Left arrow key is pressed, X =  " + player_x + " , Y = "+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function right()
+{
+	if(player_x <=850)
+    {
+        player_x = player_x + block_image_width;
+        console.log("block image width = " + block_image_width);
+        console.log("When up Right arrow key is pressed, X =  " + player_x + " , Y = "+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
